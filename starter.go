b09475885430ae80949af6aed7362896ef2e3125
Aleksey Tryapitsyn <aeriswave@outlook.com>
2021-04-18 11:38:47 +0300

    Временно объединил работающий код в одном файле

--- starter.go
+++ starter.go
@@ -7,6 +7,42 @@ import (
 	"time"
 )
 
+var ticker int = 0
+var mdText string = "Первый запуск\n\n"
+var count int = 0
+var guess int = -1
+var number int = rand.Intn(10)
+var ptik int = 0
+
+func Ticker() {
+	ticker = (ticker+1)%1000000000 - ticker%1000000000 + ticker
+	if count == 0 {
+		mdText = ""
+	}
+	if (guess < 0) || (guess > 9) {
+		guess = rand.Intn(10)
+	}
+	if count <= 0 {
+		mdText = fmt.Sprintf("Это %d?", guess) + "\n\n" + mdText
+		count = -count + 1
+	} else if guess != number {
+		if guess < number {
+			mdText = fmt.Sprintf("Нет, не %d, а больше\n", guess) + mdText
+			guess++
+		} else {
+			mdText = fmt.Sprintf("Нет, не %d, а меньше\n", guess) + mdText
+			guess--
+		}
+		count = -count
+	} else {
+		mdText = fmt.Sprintf("\nУгадал c %d попытки, это %d\n", count, number) + mdText
+		count = 0
+		guess = -1
+		number = rand.Intn(10)
+	}
+	return
+}
+
 type JSONString string
 
 type Message struct {
@@ -26,24 +62,24 @@ func Handler() ([]byte, error) {
 	default:
 		tmpTOP += fmt.Sprintf("%d-й тик!\n", (ticker >> 1))
 	}
-	tmpTOP += "\nОбнови страницу, что бы продолжить...\n\n"
+	tmpTOP += "\nОбнови страницу, что бы продолжить...\nУгадай число от 0 до 9.\n"
 
 	switch time.Now().Weekday() {
 
 	case time.Monday:
-		tmpTOP = "Сегодня понедельник." + tmpTOP
+		tmpTOP = "Сегодня понедельник.\n" + tmpTOP
 	case time.Tuesday:
-		tmpTOP = "Сегодня вторник." + tmpTOP
+		tmpTOP = "Сегодня вторник.\n" + tmpTOP
 	case time.Wednesday:
-		tmpTOP = "Сегодня среда." + tmpTOP
+		tmpTOP = "Сегодня среда.\n" + tmpTOP
 	case time.Thursday:
-		tmpTOP = "Сегодня четверг." + tmpTOP
+		tmpTOP = "Сегодня четверг.\n" + tmpTOP
 	case time.Friday:
-		tmpTOP = "Сегодня пятница." + tmpTOP
+		tmpTOP = "Сегодня пятница.\n" + tmpTOP
 	case time.Saturday:
-		tmpTOP = "Сегодня суббота." + tmpTOP
+		tmpTOP = "Сегодня суббота.\n" + tmpTOP
 	case time.Sunday:
-		tmpTOP = "Сегодня воскресенье." + tmpTOP
+		tmpTOP = "Сегодня воскресенье.\n" + tmpTOP
 	}
 
 	var tmpText string = tmpTOP + "\n\n" + mdText + "\n\n" + tmpDOWN
@@ -54,39 +90,4 @@ func Handler() ([]byte, error) {
 
 func (j JSONString) MarshalJSON() ([]byte, error) {
 	return []byte(j), nil
-}
-
-var ticker int = 0
-var mdText string = "Первый запуск\n\n"
-var count int = -1
-var guess int = rand.Intn(10)
-var number int = rand.Intn(10)
-var ptik int = 0
-
-func Ticker() {
-	ticker = (ticker+1)%1000000000 - ticker%1000000000 + ticker
-	if count == -1 {
-		guess = ticker % 10
-		number = rand.Intn(10)
-		ptik = (ticker) % 2
-	}
-	if guess != number {
-		if count < 0 {
-			mdText = fmt.Sprintf("Это %d?", guess) + "\n\n" + mdText
-			count = 1
-		} else {
-			if guess < number {
-				mdText = fmt.Sprintf("Нет, не %d, а больше\n", guess) + mdText
-				guess++
-			} else {
-				mdText = fmt.Sprintf("Нет, не %d, а меньше\n", guess) + mdText
-				guess--
-			}
-			count = -1
-		}
-	} else {
-		mdText = fmt.Sprintf("\nУгадал, это %d\n", number) + mdText
-		count = -1
-	}
-	return
 }
