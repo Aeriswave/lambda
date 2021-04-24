c75eb1d30a3fe6e5e7f9ecdf4e30e83cd152269c
Aleksey Tryapitsyn <aeriswave@outlook.com>
2021-04-24 22:59:00 +0300

    Корректировка под API

--- starter.go
+++ starter.go
@@ -7,13 +7,10 @@ import (
 	"math/rand"
 	"time"
 
-	//"lambda/lambda/tst-mod/lambda/lambda/github.com/aeriswave/jsonText/jsonText"
-
-	txt "github.com/aeriswave/jsonText"
+	txt "github.com/Aeriswave/TextFormer"
 )
 
 var ticker int = 0
-var mdText string = "Первый запуск\n\n"
 var count int = 0
 var lowNum int = -1
 var upNum int = 50
@@ -21,26 +18,42 @@ var guess int = rand.Intn(upNum - lowNum - 1)
 var number int = rand.Intn(upNum - lowNum - 1)
 var ptik int = 0
 
-func Main() {
-	//	var ttt jsonText
+func main() {
+	var tmp txt.TextString = ""
+	var MSG txt.TextTemplate = txt.TextTemplate{
+		Top:         tmp,
+		TopSplit:    tmp,
+		Middle:      tmp,
+		BottomSplit: tmp,
+		Bottom:      tmp}
+
+	//	var iMSG txt.IText = &MSG
+	MSG.Set("Угадай число.", "Для обновления необходимо перезагрузить страницу", "")
+	var bb string = *Ticker()
+	MSG.Middle = txt.TextString(bb)
+	fmt.Printf(MSG.Get())
+	//	fmt.Printf(MSG.Get())
+	//	fmt.Printf(string(MSG.Middle))
 	return
 }
 
-func Ticker() {
+func Ticker() *string {
+	var tikerText string = "Новый тикер\n\n"
+
 	ticker = (ticker+1)%120 - ticker%120 + ticker
 	if count == 0 {
-		mdText = ""
+		tikerText = ""
 	}
 	if guess < 0 {
 		guess = rand.Intn(16 + ticker)
 	}
 
 	if count <= 0 {
-		mdText = fmt.Sprintf("Это %d?\n ", guess) + mdText
+		tikerText = fmt.Sprintf("Это %d?\n ", guess) + tikerText
 		count = -count + 1
 	} else if guess != number {
 		if guess < number {
-			mdText = fmt.Sprintf("Нет, не %d, а больше\n", guess) + mdText
+			tikerText = fmt.Sprintf("Нет, не %d, а больше\n", guess) + tikerText
 			lowNum = guess
 			if lowNum >= upNum-1 {
 				upNum = (lowNum + 1) << 2
@@ -49,7 +62,7 @@ func Ticker() {
 				guess = lowNum + (upNum-lowNum)>>1
 			}
 		} else {
-			mdText = fmt.Sprintf("Нет, не %d, а меньше\n", guess) + mdText
+			tikerText = fmt.Sprintf("Нет, не %d, а меньше\n", guess) + tikerText
 			upNum = guess
 			if lowNum >= upNum-1 {
 				lowNum = -1
@@ -58,14 +71,14 @@ func Ticker() {
 		}
 		count = -count
 	} else {
-		mdText = fmt.Sprintf("\nУгадал c %d попытки, это %d\n", count, number) + mdText
+		tikerText = fmt.Sprintf("\nУгадал c %d попытки, это %d\n", count, number) + tikerText
 		count = 0
 		guess = rand.Intn(10 + lowNum + upNum>>1 + ticker)
 		upNum = 0
 		lowNum = -1
 		number = rand.Intn(10 + lowNum + upNum + ticker)
 	}
-	return
+	return &tikerText
 }
 
 type Message struct {
@@ -77,45 +90,41 @@ type Message struct {
 	Answer string
 }
 
-func Handler() ([]byte, error) {
-	var tmpTOP txt.TextString = ""
-	var tmpDOWN string = "\nУгадай число.\n"
-	Ticker()
+func ShowText() ([]byte, error) {
+	var MSG txt.TextString = " "
+	var iMSG txt.IText
+	iMSG = &MSG
+	iMSG.Set("Угадай число.", "Для обновления необходимо перезагрузить страницу", "")
+	var middleText *string = Ticker()
 	switch ticker % 2 {
 	case ptik:
-		tmpTOP.AddLowerLines("Так, сделан шаг!")
+		iMSG.Set("Так, сделан шаг!")
 	default:
-		tmpTOP.AddLowerLines(fmt.Sprintf("%d-й тик!", (ticker >> 1)))
+		iMSG.Set(fmt.Sprintf("%d-й тик!", (ticker >> 1)))
 	}
-	tmpTOP.AddLowerLines("Обнови страницу, что бы продолжить...", fmt.Sprintf("Угадай число быстрее робота: примерный диапазон %d...%d.", lowNum+1, upNum-1))
+	iMSG.AddTopUD(
+		fmt.Sprintf(
+			"Угадай число быстрее робота: примерный диапазон %d...%d.", lowNum+1, upNum-1))
+	iMSG.AddTopUD("Обнови страницу, что бы продолжить...")
 
 	switch time.Now().Weekday() {
-
 	case time.Monday:
-		tmpTOP = "Сегодня понедельник.\n" + tmpTOP
+		iMSG.AddTopUD("Сегодня понедельник.")
 	case time.Tuesday:
-		tmpTOP = "Сегодня вторник.\n" + tmpTOP
+		iMSG.AddTopUD("Сегодня вторник.")
 	case time.Wednesday:
-		tmpTOP = "Сегодня среда.\n" + tmpTOP
+		iMSG.AddTopUD("Сегодня среда.")
 	case time.Thursday:
-		tmpTOP = "Сегодня четверг.\n" + tmpTOP
+		iMSG.AddTopUD("Сегодня четверг.")
 	case time.Friday:
-		tmpTOP = "Сегодня пятница.\n" + tmpTOP
+		iMSG.AddTopUD("Сегодня пятница.")
 	case time.Saturday:
-		tmpTOP = "Сегодня суббота.\n" + tmpTOP
+		iMSG.AddTopUD("Сегодня суббота.")
 	case time.Sunday:
-		tmpTOP = "Сегодня воскресенье.\n" + tmpTOP
+		iMSG.AddTopUD("Сегодня воскресенье.")
 	}
-	tmpDOWN += "Игра функцией на Яндекс.облакЕ\n(c) Тряпицын Алексей\n"
-	//	var tmpText string = tmpTOP + "\n\n" + mdText + "\n\n" + tmpDOWN
-
-	var mmm txt.TextTemplate
-	mmm.Set("", mdText, tmpDOWN)
-	tt, err := mmm.Get()
-	if err == nil {
-		err = nil
-	}
-	m := Message{"Автор Тряпицын Алексей Васильевич", string(tt), 9057119603, "профи.сайт/АТ", fmt.Sprintf("Правильный ответ: %d", number)}
+	iMSG.Set("", "Игра функцией на Яндекс.облакЕ\n(c) Тряпицын Алексей\n", *middleText)
+	m := Message{"Автор Тряпицын Алексей Васильевич", iMSG.Get(), 9057119603, "профи.сайт/АТ", fmt.Sprintf("Правильный ответ: %d", number)}
 
 	return json.Marshal(m)
 }
